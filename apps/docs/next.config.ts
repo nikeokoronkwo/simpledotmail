669d9ca4f92d3c717745152ddace84487187509a
Nikechukwu Okoronkwo <nikechukwu@gmail.com>
2026-02-13 08:03:55 -0600

    Added microfrontends architecture for development of website apps, and worked successful turbo dev commands

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/docs", // for microfrontend application
   typescript: {
     ignoreBuildErrors: true,
   },
